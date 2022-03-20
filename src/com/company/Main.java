c7e10b0de177775524e8f5977d0bb5056c7d59a8
ANDREEA CEACA <CEACA.ANDREEA@YAHOO.COM>
2022-03-20 12:37:28 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello");
+        System.out.println("Andreea");
+        System.out.println(12+30);
+        System.out.println(36/2);
+
+
+
+    }
+}
