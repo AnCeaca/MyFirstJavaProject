210f40ffd5e124c159b39c63f848822ebed616c4
ANDREEA CEACA <CEACA.ANDREEA@YAHOO.COM>
2022-04-07 07:50:56 +0100

    Tema Lab 11.

--- src/com/company/calculator/LogicalOperations.java
+++ src/com/company/calculator/LogicalOperations.java
@@ -32,7 +32,50 @@ public class LogicalOperations {
         } else if (!text.equals("FastTrack") && number >= 4) {
             return number + text;
         } else {
-            return "";
+            return ".";
+        }
+    }
+
+    public String snow(int number){
+        if (number > 8 || number ==6){
+            return "The amount of snow this winter was(cm): " + number;
+        } else {
+            return "The forecast snow is(cm) " + number;
+        }
+    }
+
+    public String greater (int number){
+        if (number>3 && !(number ==4)){
+            return "The number is greater than 3 and not equal to 4";
+        } else if (number == 4){
+            return "The number is equal to 4";
+        }else if (number <3){
+            return "The number is lower than 3";
+        }else {
+            return "Nothing";
+        }
+    }
+
+    // ex 8
+    // ex 9
+    //ex 10
+
+    public int biggerNumber (int first, int second, int third){
+        if (first > second){
+            return first;
+        } else if (first > third){
+            return first;
+        }else if (second > third){
+            return second;
+        }else if (second > first){
+            return second;
+        }else if (third > first){
+            return third;
+        }else {
+            return third;
         }
     }
 }
+
+
+
--- src/com/company/calculator/Main3.java
+++ src/com/company/calculator/Main3.java
@@ -6,8 +6,28 @@ public class Main3 {
         Calculator calc = new Calculator();
         LogicalOperations op = new LogicalOperations();
 
+        System.out.println(calc.divide(21,3));
+        System.out.println(calc.modulus(7,9));
+        System.out.println(calc.multiply(25,6));
+        System.out.println(calc.sum(7,9));
+        System.out.println(calc.substract(10,3));
+
+
         int checkBiggest = op.checkBiggerNumber(78, 56);
         System.out.println("The bigger number is " + checkBiggest);
+
+        System.out.println(op.getMax(45,13));
+
+        System.out.println(op.verifyTextFastTrack("FastTrack"));
+
+        System.out.println(op.verifyTextAndNumber("FastTrack", 5));
+
+        System.out.println(op.snow(9));
+
+        System.out.println(op.greater(5));
+
+        System.out.println(op.biggerNumber(9,5,10));
+
 
     }
 
