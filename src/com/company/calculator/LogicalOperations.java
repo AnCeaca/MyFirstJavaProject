fe6f5d145cf9f75fee28ed324aa35aa23286000e
ANDREEA CEACA <CEACA.ANDREEA@YAHOO.COM>
2022-04-13 19:13:45 +0100

    Tema Lab 12 For Loop

--- src/com/company/calculator/LogicalOperations.java
+++ src/com/company/calculator/LogicalOperations.java
@@ -2,6 +2,8 @@ package com.company.calculator;
 
 public class LogicalOperations {
 
+    //ex 3
+
     public int checkBiggerNumber(int first, int second){
         if(first > second){
             return first;
@@ -18,6 +20,8 @@ public class LogicalOperations {
         }
     }
 
+    //ex 4
+
     public  String verifyTextFastTrack(String text) {
         if (text.equals("FastTrack")) {
             return "Learning text comparison";
@@ -25,6 +29,8 @@ public class LogicalOperations {
             return "Got to try some more";
         }
     }
+
+    //ex 5
 
     public  String verifyTextAndNumber(String text, int number){
         if (text.equals("FastTrack") && number <= 3) {
@@ -36,6 +42,8 @@ public class LogicalOperations {
         }
     }
 
+    //ex 6
+
     public String snow(int number){
         if (number > 8 || number ==6){
             return "The amount of snow this winter was(cm): " + number;
@@ -43,6 +51,8 @@ public class LogicalOperations {
             return "The forecast snow is(cm) " + number;
         }
     }
+
+    //ex 7
 
     public String greater (int number){
         if (number>3 && !(number ==4)){
@@ -57,8 +67,97 @@ public class LogicalOperations {
     }
 
     // ex 8
+
+    public void printNumber (int number){
+        switch (number) {
+
+            case 0:
+
+                System.out.println("The number os 0");
+
+                break;
+
+            case 1:
+
+                System.out.println("The number is 1");
+
+                break;
+
+            case 2:
+
+                System.out.println("The number is 2");
+
+                break;
+
+            case 3:
+
+                System.out.println("The number is 3");
+
+                break;
+
+            case 4:
+
+                System.out.println("The number is 4");
+
+                break;
+
+            case 5:
+
+                System.out.println("The number is 5");
+
+                break;
+
+            case 6:
+
+                System.out.println("The number is 6");
+
+                break;
+
+            case 7:
+
+                System.out.println("The number is 7");
+
+                break;
+
+            case 8:
+
+                System.out.println("The number is 8");
+
+                break;
+
+            case 9:
+
+                System.out.println("The number is 9");
+
+                break;
+
+            default:
+
+                System.out.println("Not allowed");
+
+        }
+    }
+
     // ex 9
+
+    public boolean isNumberEven (int number){
+        if (number %2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
     //ex 10
+
+    public boolean isEligibleToVote (int number){
+        if (number >= 18){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    //ex 11 (LAB 11)
 
     public int biggerNumber (int first, int second, int third){
         if (first > second){
@@ -75,6 +174,95 @@ public class LogicalOperations {
             return third;
         }
     }
+
+
+
+
+
+    //ex 1 (LAB 12)
+
+    public void countTo (int x){
+        for (int i = x; i <=100; i++){
+            System.out.println(i);
+        }
+    }
+    // ex 2 (LAB 12)
+
+    public void countBack (int x){
+        for (int i = x; i>=-100; i--){
+            System.out.println(i);
+        }
+    }
+    //ex 3 (LAB 12)
+
+    public void countTwo (int first, int second){
+        for (int i = first; i>=second; i--){
+            System.out.println(i);
+        }
+    }
+    //ex 4 (LAB 12)
+
+   /* public void minMax(int x, int y){
+        for (int i = x; i>=y; i++){
+            System.out.println(i);
+        }for (int i = y; i>= x; i++){
+            System.out.println(i);
+        }
+    }
+
+    */
+
+
+    //ex 5 (LAB 12)
+
+    public void evenNumbers (int x){
+        for (int i = 1; i<=x; i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+    //ex 6 (LAB 12)
+
+    public void oddNumbers (int x){
+        for (int i = 1; i<=x; i++){
+            if (i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+    //ex 7 (LAB 12)
+
+    public void printSum(int x, int y){
+        int sum = 0;
+        for(int i = x; i <= y; i++){
+            sum = i + sum;
+        }
+        System.out.println(sum);
+    }
+    //ex 8 (LAB 12)
+
+    public void printSumAndAverage(int x, int y){
+        int sum = 0;
+        for(int i = x; i <= y; i++){
+            sum = i + sum;
+        }
+        System.out.println(sum/(y-x+1f));
+    }
+    //ex 9 (LAB 12)
+
+    public void pattern(int x, int y){
+        for(int i = 7; i >= 1; i--) {
+            for(int j=1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
 }
 
 
--- src/com/company/calculator/Main3.java
+++ src/com/company/calculator/Main3.java
@@ -3,33 +3,57 @@ package com.company.calculator;
 public class Main3 {
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
+     //   Calculator calc = new Calculator();
         LogicalOperations op = new LogicalOperations();
 
-        System.out.println(calc.divide(21,3));
-        System.out.println(calc.modulus(7,9));
-        System.out.println(calc.multiply(25,6));
-        System.out.println(calc.sum(7,9));
-        System.out.println(calc.substract(10,3));
+     //   System.out.println(calc.divide(21,3));
+     //   System.out.println(calc.modulus(7,9));
+     //   System.out.println(calc.multiply(25,6));
+     //   System.out.println(calc.sum(7,9));
+     //   System.out.println(calc.substract(10,3));
 
 
-        int checkBiggest = op.checkBiggerNumber(78, 56);
-        System.out.println("The bigger number is " + checkBiggest);
+     //   int checkBiggest = op.checkBiggerNumber(78, 56);
+     //   System.out.println("The bigger number is " + checkBiggest);
+     //   System.out.println(op.getMax(45,13));
+     //   System.out.println(op.verifyTextFastTrack("FastTrack"));
 
-        System.out.println(op.getMax(45,13));
+     //   System.out.println(op.verifyTextAndNumber("FastTrack", 5));
 
-        System.out.println(op.verifyTextFastTrack("FastTrack"));
+     //   System.out.println(op.snow(9));
 
-        System.out.println(op.verifyTextAndNumber("FastTrack", 5));
+    //    System.out.println(op.greater(5));
 
-        System.out.println(op.snow(9));
+    //    System.out.println(op.biggerNumber(9,5,10));
 
-        System.out.println(op.greater(5));
+        op.countTo(60);
+        System.out.println(" ");
+        op.countBack(5);
+        System.out.println(" ");
+        op.countTwo(20,5);
+        System.out.println(" ");
+      //  op.minMax(7,6);
+        System.out.println(" ");
+        op.evenNumbers(100);
+        System.out.println(" ");
+        op.oddNumbers(100);
+        System.out.println(" ");
+        op.printSum(1,100);
+        System.out.println(" ");
+        op.printSumAndAverage(1,100);
+        System.out.println(" ");
+        op.pattern(8,1);
 
-        System.out.println(op.biggerNumber(9,5,10));
+
+
+
+
+
+
+
+
+
 
 
     }
-
-
 }
