30df5d8c79d3338b5a8f6eb112cc77b19f3a0502
ANDREEA CEACA <CEACA.ANDREEA@YAHOO.COM>
2022-05-05 10:13:40 +0100

    Tema Lab 15 - Try-Catch (class Read)

---
+++ src/com/company/calculator/Main5.java
@@ -0,0 +1,47 @@
+package com.company.calculator;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main5 {
+
+    public static void main(String[] args) {
+
+//        int[] myArray = new int[]{3, 4, 5};
+//        try {
+//            System.out.println(myArray[4]);
+//        } catch (IndexOutOfBoundsException e){
+//            System.out.println(e.getMessage());
+//            System.err.println("Incorrect value!");
+//        }
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("Enter a number: ");
+//        try{
+//            int position = scan.nextInt();
+//            System.out.println(myArray[position]);
+//        }catch (InputMismatchException e){
+//            System.err.println("Invalid value!");
+//        }
+//
+//        List<Integer> list = new ArrayList<>();
+//        list.add(3);
+//        System.out.println(list.get(3));
+
+        Read read = new Read();
+//        System.out.println(read.getInt());
+//        System.out.println(read.getDouble());
+//        System.out.println(read.getFloat());
+//        System.out.println(read.getString);
+//        System.out.println(read.getArray());
+//        System.out.println(read.getList());
+
+
+
+
+
+    }
+}
